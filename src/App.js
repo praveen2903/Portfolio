5021195dfb5464a5aadb5458d427ccfbfe8d75a6
praveen2903 <vvit20bq1a05l0@gmail.com>
2023-12-11 19:45:08 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { AnimatePresence } from 'framer-motion';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
+import AppFooter from './components/shared/AppFooter';
+import AppHeader from './components/shared/AppHeader';
+import './css/App.css';
+import UseScrollToTop from './hooks/useScrollToTop';
+
+const About = lazy(() => import('./pages/AboutMe'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+const Home = lazy(() => import('./pages/Home'));
+const Projects = lazy(() => import('./pages/Projects'));
+const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<AnimatePresence>
+			<div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+				<Router>
+					<ScrollToTop />
+					<AppHeader />
+					<Suspense fallback={""}>
+						<Routes>
+							<Route path="/" element={<Home />} />
+							<Route path="projects" element={<Projects />} />
+							<Route
+								path="projects/single-project"
+								element={<ProjectSingle />}
+							/>
+
+							<Route path="about" element={<About />} />
+							<Route path="contact" element={<Contact />} />
+						</Routes>
+					</Suspense>
+					<AppFooter />
+				</Router>
+				<UseScrollToTop />
+			</div>
+		</AnimatePresence>
+	);
 }
 
 export default App;
---
+++ src/components/shared/AppFooterCopyright.jsx
@@ -0,0 +1,19 @@
+function AppFooterCopyright() {
+	return (
+		<div className="font-general-regular flex justify-center items-center text-center">
+			<div className="text-lg text-ternary-dark dark:text-ternary-light">
+				&copy; {new Date().getFullYear()}
+				<a
+					href="https://github.com/praveen2903/Portfolio"
+					target="__blank"
+					className="hover:underline hover:text-indigo-600 dark:hover:text-indigo-300 ml-1 duration-500"
+				>
+					Rokkam Sai Praveen Portfolio
+				</a>
+				
+			</div>
+		</div>
+	);
+}
+
+export default AppFooterCopyright;
