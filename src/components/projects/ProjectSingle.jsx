1be98dd2a672b373614bd16c0f2ff58224b969c2
praveen2903 <vvit20bq1a05l0@gmail.com>
2023-12-14 14:08:03 +0530

    done many changes of ui

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -4,34 +4,34 @@ import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 
 const ProjectSingle = ({image,title,description,gitlink,deploylink,icons}) => {
   return (
-        <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="group relative flex flex-col flex-wrap h-80 w-[95%] mx-auto shadow-xl rounded-xl ">
-          <img src={image} alt={title} className="h-80 rounded-xl"/>
-          <div className="absolute flex flex-col justify-center items-center bottom-0 left-0 right-0 h-0 overflow-hidden group-hover:h-full transition-[height_0.5s] duration-500 bg-darkblue rounded-xl">
-            <h3 data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="text-2xl font-medium text-white my-2">{title}</h3>
-            <p data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="px-2 text-lg text-white">{description}</p>
-            <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="flex flex-wrap items-center gap-3 mt-3">
-            {
-              icons.map((Icon,index)=>(
-                <div className="rounded-full p-2" key={index}>
-                  <Icon className="text-white text-4xl font-bold"/>
-                </div>
-              ))
-            }
+      <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="group relative flex flex-col flex-wrap h-80 w-[95%] mx-auto shadow-xl rounded-xl mt-6">
+      <img src={image} alt={title} className="h-80 rounded-xl"/>
+      <div className="absolute flex flex-col justify-center items-center bottom-0 left-0 right-0 h-0 overflow-hidden group-hover:h-full transition-[height_0.5s] duration-500 bg-[#3b0764] rounded-xl">
+        <h3 data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="text-2xl font-medium text-white my-2">{title}</h3>
+        <p data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="px-2 text-lg text-white text-justify p-1">{description}</p>
+        <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="flex flex-wrap items-center gap-3 mt-3">
+        {
+          icons.map((Icon,index)=>(
+            <div className="rounded-full p-2" key={index}>
+              <Icon className="text-white text-4xl font-bold"/>
             </div>
-            <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="flex gap-7 justify-center items-center my-7 text-2xl">
-              <div className="bg-white rounded-full p-2">
-                <a className="text-darkblue text-xl bg-white" href={gitlink} target="_blank" rel="noopener noreferrer">
-                  <FaGithub />
-                </a>
-              </div>
-              <div className="bg-white rounded-full p-2">
-              <a className="text-darkblue text-xl bg-white" href={deploylink} target="_blank" rel="noopener noreferrer">
-                <FaExternalLinkAlt  className='p-[1px]'/>
-              </a>
-              </div>
-            </div>
+          ))
+        }
+        </div>
+        <div data-aos="zoom-in-up" data-aos-duration="1000" data-aos-once="false" className="flex gap-7 justify-center items-center my-7 text-2xl">
+          <div className="bg-white rounded-full p-2">
+            <a className="text-darkblue text-xl bg-white" href={gitlink} target="_blank" rel="noopener noreferrer">
+              <FaGithub />
+            </a>
+          </div>
+          <div className="bg-white rounded-full p-2">
+          <a className="text-darkblue text-xl bg-white" href={deploylink} target="_blank" rel="noopener noreferrer">
+            <FaExternalLinkAlt  className='p-[1px]'/>
+          </a>
           </div>
         </div>
+      </div>
+    </div>
   );
 }
 
--- src/components/projects/ProjectsGrid.jsx
+++ src/components/projects/ProjectsGrid.jsx
@@ -5,49 +5,39 @@ import { ProjectsContext } from '../../context/ProjectsContext';
 import ProjectsFilter from './ProjectsFilter';
 
 const ProjectsGrid = () => {
-	const {
-		projects,
-		searchProject,
-		setSearchProject,
-		searchProjectsByTitle,
-		selectProject,
-		setSelectProject,
-		selectProjectsByCategory,
-	} = useContext(ProjectsContext);
+    const {
+        projects,
+        searchProject,
+        setSearchProject,
+        searchProjectsByTitle,
+        selectProject,
+        setSelectProject,
+        selectProjectsByCategory,
+    } = useContext(ProjectsContext);
 
-	return (
-		<section className="py-5 sm:py-10 mt-5 sm:mt-10">
-			<div className="text-center">
-				<p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
-					Projects portfolio
-				</p>
-			</div>
+    return (
+        <section className="py-5 sm:py-10 mt-5 sm:mt-10">
+            <div className="text-center">
+                <p className="font-general-medium text-2xl sm:text-4xl mb-1 text-ternary-dark dark:text-ternary-light">
+                    Projects portfolio
+                </p>
+            </div>
 
-			<div className="mt-10 sm:mt-16">
-				<h3
-					className="font-general-regular 
+            <div className="mt-10 sm:mt-16">
+                <h3
+                    className="font-general-regular 
                         text-center text-secondary-dark
                         dark:text-ternary-light
                         text-md
                         sm:text-xl
-                        mb-3
-                        "
-				>
-					Search projects by title or filter by category
-				</h3>
-				<div
-					className="
-                        flex
-                        justify-between
-                        border-b border-primary-light
-                        dark:border-secondary-dark
-                        pb-3
-                        gap-3
-                        "
-				>
-					<div className="flex justify-between gap-2">
-						<span
-							className="
+                        mb-3"
+                >
+                    Search projects by title or filter by category
+                </h3>
+                <div className="sm:flex sm:justify-between sm:items-center mt-8">
+                    <div className="sm:flex sm:gap-2">
+                        <span
+                            className="
                                 hidden
                                 sm:block
                                 bg-primary-light
@@ -57,20 +47,20 @@ const ProjectsGrid = () => {
                                 rounded-xl
                                 cursor-pointer
                                 "
-						>
-							<FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
-						</span>
-						<input
-							onChange={(e) => {
-								setSearchProject(e.target.value);
-							}}
-							className="font-general-medium 
+                        >
+                            <FiSearch className="text-ternary-dark dark:text-ternary-light w-5 h-5"></FiSearch>
+                        </span>
+                        <input
+                            onChange={(e) => {
+                                setSearchProject(e.target.value);
+                            }}
+                            className="font-general-medium 
                                 pl-3
                                 pr-1
                                 sm:px-4
                                 py-2
                                 border 
-                            border-gray-200
+                                border-gray-200
                                 dark:border-secondary-dark
                                 rounded-lg
                                 text-sm
@@ -80,58 +70,60 @@ const ProjectsGrid = () => {
                                 text-primary-dark
                                 dark:text-ternary-light
                                 "
-							id="name"
-							name="name"
-							type="search"
-							required=""
-							placeholder="Search Projects"
-							aria-label="Name"
-						/>
-					</div>
+                            id="name"
+                            name="name"
+                            type="search"
+                            required=""
+                            placeholder="Search Projects"
+                            aria-label="Name"
+                        />
+                    </div>
 
-					<ProjectsFilter setSelectProject={setSelectProject} />
-				</div>
-			</div>
+                    <div className="mt-4 sm:mt-0">
+                        <ProjectsFilter setSelectProject={setSelectProject} />
+                    </div>
+                </div>
+            </div>
 
-			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
-				{selectProject
-					? selectProjectsByCategory.map((project) => (
-							<ProjectSingle
-								image={project.image}
-								title={project.title}
-								description={project.description}
-								gitlink={project.gitlink}
-								deploylink={project.deploylink}
-								icons={project.icons}
-								key={project.id}
-							/>
-					  ))
-					: searchProject
-					? searchProjectsByTitle.map((project) => (
-							<ProjectSingle
-								image={project.image}
-								title={project.title}
-								description={project.description}
-								gitlink={project.gitlink}
-								deploylink={project.deploylink}
-								icons={project.icons}
-								key={project.id}
-							/>
-					  ))
-					: projects.map((project) => (
-							<ProjectSingle
-								image={project.image}
-								title={project.title}
-								description={project.description}
-								gitlink={project.gitlink}
-								deploylink={project.deploylink}
-								icons={project.icons}
-								key={project.id}
-							/>
-					  ))}
-			</div>
-		</section>
-	);
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-6 sm:gap-10">
+                {selectProject
+                    ? selectProjectsByCategory.map((project) => (
+                        <ProjectSingle
+                            image={project.image}
+                            title={project.title}
+                            description={project.description}
+                            gitlink={project.gitlink}
+                            deploylink={project.deploylink}
+                            icons={project.icons}
+                            key={project.id}
+                        />
+                    ))
+                    : searchProject
+                        ? searchProjectsByTitle.map((project) => (
+                            <ProjectSingle
+                                image={project.image}
+                                title={project.title}
+                                description={project.description}
+                                gitlink={project.gitlink}
+                                deploylink={project.deploylink}
+                                icons={project.icons}
+                                key={project.id}
+                            />
+                        ))
+                        : projects.map((project) => (
+                            <ProjectSingle
+                                image={project.image}
+                                title={project.title}
+                                description={project.description}
+                                gitlink={project.gitlink}
+                                deploylink={project.deploylink}
+                                icons={project.icons}
+                                key={project.id}
+                            />
+                        ))}
+            </div>
+        </section>
+    );
 };
 
 export default ProjectsGrid;
